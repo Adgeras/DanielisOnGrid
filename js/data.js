d60bdba4089360c248519f927ac8dd0a714802f2
Edgariukstis <edgaras.banaitis@gmail.com>
2020-05-29 14:14:57 +0300

    pries progress js jungima

---
+++ js/data.js
@@ -0,0 +1,17 @@
+/*for progress bars section */
+
+const skills = [
+    { 
+        title: 'Web design',
+        value: 95
+    },
+    { 
+        title: 'Branding',
+        value: 80
+    },
+    { 
+        title: 'Develpoment',
+        value: 90
+    }
+
+];
