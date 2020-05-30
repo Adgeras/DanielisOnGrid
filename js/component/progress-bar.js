3fd346de204b6c8699b780eefab5773ddc630863
Edgariukstis <edgaras.banaitis@gmail.com>
2020-05-30 10:54:09 +0300

    working js progressBar with full animation

--- js/component/progress-bar.js
+++ js/component/progress-bar.js
@@ -3,9 +3,15 @@
 function renderProgressBar (selector, data) {
     // validacija
     const DOM = document.querySelector(selector);
-    
+       
     if (!DOM) {
         throw 'ERROR: could not find element to render new content.'
+    }
+    if (!Array.isArray(data)) {
+        throw 'ERROR: need some data (array format).'
+    }
+    if (data.length === 0){
+        throw 'ERROR: need some data (not empty array).'
     }
 
     //logika:
@@ -15,16 +21,32 @@ function renderProgressBar (selector, data) {
     let HTML = '';
     for (let i = 0; i < data.length; i++) {
         const skill = data[i];
+        // validacija
+        if (typeof(skill) !== 'object') {
+            continue;
+        }
+        if (typeof(skill.title) !== 'string' || skill.title === '') {
+            continue;
+        }
+        if (!isFinite(skill.value) || skill.value < 0 || skill.value > 100) {
+            continue; 
+        }
+
+
+        // elemento logika
         HTML += `
             <div class="progress-bar" data-animated="false"
             data-value="${skill.value}%">
                 <div class="bar">
                     <div class="progress" style="width: 0%" >
                         <div class="label">${skill.title}</div>
-                        <div class="value">${skill.value}%</div>
+                        <div class="value">0%</div>
                     </div>
                 </div>
             </div>`;
+    }
+    if (HTML === '') {
+        throw 'ERROR: totally messed up dude... no correct data';
     }
     
     //rezultato grazinimas
@@ -33,4 +55,45 @@ function renderProgressBar (selector, data) {
         return;
 }
 
-export default renderProgressBar;
+function animateProgressBars(progressBarsSelector){
+    const progressBars = document.querySelectorAll(progressBarsSelector + '> .progress-bar');
+    
+    window.addEventListener('scroll', () => {
+        const windowBottomHeight = window.scrollY + window.innerHeight;
+    
+        for (let i = 0; i < progressBars.length; i++) {            
+            const pg = progressBars[i];   
+            const progress = pg.querySelector('.progress'); 
+            const DOMvalue = progress.querySelector('.value');
+            const pgBottomHeight = pg.offsetTop + pg.offsetHeight;
+            
+            if (pgBottomHeight < windowBottomHeight  &&
+                pg.dataset.animated === 'false') {
+                pg.dataset.animated = 'true';
+                progress.style.width = pg.dataset.value;
+                animateProgressBarTextvalue(DOMvalue, pg.dataset.value);   
+            }   
+        }
+    });
+   
+    function animateProgressBarTextvalue(element, value) {
+        const number = parseInt(value); // final number to reach
+        const time = 3000; // miliseconds. Max 60 frames per seconds
+        const fps = 30; //30 f-rames p-er s-econd
+        const refreshTime = 1000 / fps;
+        const totalSteps = time / refreshTime;
+        let step = 0;
+
+        const timer = setInterval(() => {
+            step++;
+            element.innerText = Math.round(step / totalSteps * number)+`%`;
+            if (step >= totalSteps) {
+                clearInterval(timer);
+            }
+        }, refreshTime);       
+    }
+}
+
+export default {renderProgressBar, 
+                animateProgressBars
+} ;
--- js/home.js
+++ js/home.js
@@ -2,28 +2,15 @@
 
 import skills from './data/skills.js';
 
-import renderProgressBar from './component/progress-bar.js'
+import progressBars from './component/progress-bar.js'
+
+console.log(progressBars);
 
 const progressBarsSelector = '#skills';
-renderProgressBar(progressBarsSelector, skills);
+progressBars.renderProgressBar(progressBarsSelector, skills);
+progressBars.animateProgressBars(progressBarsSelector);
 
-const progressBars = document.querySelectorAll(progressBarsSelector + '> .progress-bar');
 
 
-window.addEventListener('scroll', () => {
-    const windowBottomHeight = window.scrollY + window.innerHeight;
 
-    for (let i = 0; i < progressBars.length; i++) {            
-        const pg = progressBars[i];   
-        const progress = pg.querySelector('.progress'); 
-        const pgBottomHeight = pg.offsetTop + pg.offsetHeight;
-        
-        if (pgBottomHeight < windowBottomHeight  &&
-            pg.dataset.animated === 'false') {
-            pg.dataset.animated = 'true';
-            progress.style.width = pg.dataset.value;
-            
-        }   
-    }
-});
 
