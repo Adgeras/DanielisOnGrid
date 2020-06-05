0a18a5274f6a9c4c220811d933fdbfb24323c953
Edgariukstis <edgaras.banaitis@gmail.com>
2020-06-05 22:26:51 +0300

    running numbers works

--- js/component/runningNumbers.js
+++ js/component/runningNumbers.js
@@ -5,57 +5,36 @@ function renderRunningNumbers (selector, data) {
     
     let HTML = '';
     for (let i = 0; i < data.length; i++) {
-        const icon = data[i].icon
-        const label = data[i].label
-        //const number = data[i].number
+        const icon = data[i].icon;
+        const label = data[i].label;
+        //const number = data[i].number;
         HTML += `
             <div class="col-3 col-12-sm">
                 <i class="fa fa-${icon}" aria-hidden="true"></i>
                 <div class="number">0</div>
                 <p>${label}</p>        
-            </div>`;      
-            
+            </div>`;        
         }
         placeForRunningNumbers.innerHTML = HTML;
-        animateNumber(selector, data);
-        
-    // numberiu animacijai reikia cikle kreiptis i funkcija 
-    // paduodant jai elementa (jo vieta) ir jo dataset reiksme "value"
-            
-    function animateNumber(selectors, data) {
-        const speed = 200;
-        //const fps = 30;
-        //const refreshTime = 1000 / fps; //laikas milisekundem per kiek turi atsinaujint
-        //const totalSteps = time / refreshTime; // kiek viso duotame laike žingsnių
-        
-        var animNumPlace = placeForRunningNumbers.querySelectorAll('.col-3 >.number ');
-        console.log(animNumPlace); // vieta pagauna 
-        console.log(animNumPlace.length) // ilgis geras
-        for (let i = 0; i <= animNumPlace.length; i++) {
-            var counter = 0;
-            const skaicius = data[i].number // sita pagauna 
-            const inc = skaicius / speed;
-            console.log(inc);            
-            //console.log(animNumPlace[i]);
-            //console.log('Reiksme iki kurios turi suktis = ' + skaicius);
-            
-            
-
-
-
-
-
-
-            // const timer = setInterval(() => {
-            //     counter += inc;
-            //     animNumPlace[i].innerText = Math.ceil(counter);  
-            //     //console.log(animNumPlace[i]);
-            //     if (counter >= skaicius) {
-            //         clearInterval(timer);
-            //     }
-            // }, refreshTime); 
-        }    
-            return;                
-    }
+                 
+                    const speed = 300;  
+                    var animNumPlace = placeForRunningNumbers.querySelectorAll('.col-3 >.number ');
+                    
+                    animNumPlace.forEach(function (a,b){
+                        function updateCount() {                                 
+                        const target = data[b].number;
+                        const count = +a.innerText;
+                        const inc = target / speed;
+                                                
+                        if (count < target) {
+                            a.innerText = Math.ceil(count + inc);
+                            setTimeout(updateCount, 1);
+                        } else {
+                            a.innerText = target;
+                            }
+                        }
+                        updateCount();
+                    });
+               
 }
 export default renderRunningNumbers;
